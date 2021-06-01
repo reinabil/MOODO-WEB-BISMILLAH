2639e6a0c4cc05f4d06aaa4dfddcd8e5215cbdfc
Hana Tasnim <hanatasnim@apps.ipb.ac.id>
2021-06-01 23:45:15 +0800

    kita koleb
    Co-authored-by: reinabil <reinabil@users.noreply.github.com>

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,12 +3,13 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
+        <a href="/">LINK KE WELCOME</a>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                
             </div>
         </div>
     </div>
